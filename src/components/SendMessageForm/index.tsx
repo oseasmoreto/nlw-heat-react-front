38a7fb57039a9a3e5585ec05740ad69804861b64
Oseas Moreto <oseas_moreto@hotmail.com>
2021-10-20 12:40:51 -0300

    Envia as mensagens para a API

--- src/components/SendMessageForm/index.tsx
+++ src/components/SendMessageForm/index.tsx
@@ -1,10 +1,22 @@
-import { useContext } from 'react';
+import { useContext, useState, FormEvent } from 'react';
 import { VscGithubInverted, VscSignOut } from 'react-icons/vsc';
 import { AuthContext } from '../../contexts/auth';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 
 export function SendMessageForm() {
   const { user, signOut } = useContext(AuthContext);
+  const [message, setMessage] = useState('');
+
+  async function handleSendMessage(event: FormEvent){
+    event.preventDefault();
+    if(!message.trim()) {
+      return;
+    }
+
+    await api.post('messages', {message});
+    setMessage('');
+  }
 
 
   return (
@@ -19,10 +31,14 @@ export function SendMessageForm() {
         <span className={styles.userGithub}><VscGithubInverted size="16" />{user?.login}</span>
       </header>
 
-      <form className={styles.sendMessageForm}>
+      <form onSubmit={handleSendMessage} className={styles.sendMessageForm}>
         <label htmlFor="message">Mensagem</label>
-        <textarea name="message" id="message" placeholder="Qual sua expectativa para o evento?">
-
+        <textarea 
+        onChange={event => setMessage(event.target.value)} 
+        name="message" 
+        id="message" 
+        value={message}
+        placeholder="Qual sua expectativa para o evento?">
         </textarea>
 
         <button type="submit">Enviar mensagem</button>
--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -40,6 +40,8 @@ export function AuthProvider(props: AuthProvider){
     })
 
     const { token, user } = response.data;
+    
+    api.defaults.headers.common.authorization = 'Bearer ' + token;
 
     localStorage.setItem('@dowhile:token', token);
 
