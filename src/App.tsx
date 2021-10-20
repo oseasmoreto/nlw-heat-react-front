ff610f32b4887a42a61795ffe200fb53c3a832b2
Oseas Moreto <oseas_moreto@hotmail.com>
2021-10-20 10:11:58 -0300

    Cria component LoginBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import styles from './App.module.scss';
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
+
+export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
   )
 }
-
-export default App
