4dba4428b090b9a074bdae801a9222c7b869d39d
Oseas Moreto <oseas_moreto@hotmail.com>
2021-10-20 13:06:39 -0300

    Adição do socket para recuperar as mensagens dinamicamente

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -5,6 +5,8 @@ import logoImg from '../../assets/logo.svg';
 import { api } from '../../services/api';
 import { useEffect, useState } from 'react';
 
+import { io } from 'socket.io-client'
+
 type Message = {
   id: string;
   text: string;
@@ -14,8 +16,30 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if(messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3000)
+  }, [])
 
   useEffect(() => {
     api.get<Message[]>('/messages/last3').then((response) => {
